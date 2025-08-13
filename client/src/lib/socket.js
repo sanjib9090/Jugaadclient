c60681b8b6224cf2487258b59a8b4089e9225557
Sanjib Barada <sanjibbarad6@gmail.com>
2025-08-13 10:30:06 +0530

    socket update

--- client/src/lib/socket.js
+++ client/src/lib/socket.js
@@ -24,7 +24,7 @@ export async function getSocket() {
   connectingPromise = (async () => {
     currentToken = await getFreshIdToken();
 
-    socket = io(import.meta.env.VITE_SOCKET_URL ?? 'https://jugaadapi.onrender.com/', {
+    socket = io(import.meta.env.VITE_SOCKET_URL ?? 'https://jugaadapi.onrender.com', {
       transports: ['websocket'],
       auth: { token: currentToken },
       autoConnect: true,
